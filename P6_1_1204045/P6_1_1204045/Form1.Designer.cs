c0a57dabeda8caf2d2ab7e260644ed6aedb24272
daffaulhaq <udaffa09@gmail.com>
2021-12-08 22:10:07 +0700

    first commit

---
+++ P6_1_1204045/P6_1_1204045/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace P6_1_1204045
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdProdi = new System.Windows.Forms.TextBox();
+            this.txtNamaProdi = new System.Windows.Forms.TextBox();
+            this.txtSingkatan = new System.Windows.Forms.TextBox();
+            this.txtKaProdi = new System.Windows.Forms.TextBox();
+            this.txtSekProdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form Input Prodi";
+            // 
+            // txtIdProdi
+            // 
+            this.txtIdProdi.Location = new System.Drawing.Point(232, 94);
+            this.txtIdProdi.Name = "txtIdProdi";
+            this.txtIdProdi.Size = new System.Drawing.Size(73, 20);
+            this.txtIdProdi.TabIndex = 1;
+            // 
+            // txtNamaProdi
+            // 
+            this.txtNamaProdi.Location = new System.Drawing.Point(232, 135);
+            this.txtNamaProdi.Name = "txtNamaProdi";
+            this.txtNamaProdi.Size = new System.Drawing.Size(207, 20);
+            this.txtNamaProdi.TabIndex = 2;
+            // 
+            // txtSingkatan
+            // 
+            this.txtSingkatan.Location = new System.Drawing.Point(232, 180);
+            this.txtSingkatan.Name = "txtSingkatan";
+            this.txtSingkatan.Size = new System.Drawing.Size(100, 20);
+            this.txtSingkatan.TabIndex = 3;
+            // 
+            // txtKaProdi
+            // 
+            this.txtKaProdi.Location = new System.Drawing.Point(232, 223);
+            this.txtKaProdi.Name = "txtKaProdi";
+            this.txtKaProdi.Size = new System.Drawing.Size(207, 20);
+            this.txtKaProdi.TabIndex = 4;
+            // 
+            // txtSekProdi
+            // 
+            this.txtSekProdi.Location = new System.Drawing.Point(232, 272);
+            this.txtSekProdi.Name = "txtSekProdi";
+            this.txtSekProdi.Size = new System.Drawing.Size(207, 20);
+            this.txtSekProdi.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(209, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "ID Prodi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(123, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Nama Prodi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(123, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Singkatan";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(123, 229);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Ketua Prodi";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(123, 279);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Sekretaris Prodi";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(144, 339);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Submit";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(354, 339);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 412);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSekProdi);
+            this.Controls.Add(this.txtKaProdi);
+            this.Controls.Add(this.txtSingkatan);
+            this.Controls.Add(this.txtNamaProdi);
+            this.Controls.Add(this.txtIdProdi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdProdi;
+        private System.Windows.Forms.TextBox txtNamaProdi;
+        private System.Windows.Forms.TextBox txtSingkatan;
+        private System.Windows.Forms.TextBox txtKaProdi;
+        private System.Windows.Forms.TextBox txtSekProdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ P6_1_1204045/P6_1_1204045/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace P6_1_1204045
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Method update dengan parameter cmd
+        private void UpdateDB(string cmd)
+        {
+            try
+            {
+                //koneksi ke database
+                //string connectionString = "Data Source=LAPTOP-2TQJ2POM\P6_1204045;Initial Catalog=P6_1204045;Integrated Security=True";
+
+                //objek baru connectionString
+                SqlConnection myConnection = new SqlConnection(@"Data Source=LAPTOP-2TQJ2POM\P6_1204045;Initial Catalog=P6_1204045;Integrated Security=True");
+
+                //membuka koneksi dengan objek connectionString
+                myConnection.Open();
+
+                //objek baru myCommand, inisialisasi dari class SqlCommand
+                SqlCommand myCommand = new SqlCommand();
+
+                //menetapkan koneksi basisdata, yaitu myConnection
+                myCommand.Connection = myConnection;
+
+                //mengatur query yang digunakan, diambil dari parameter cmd
+                myCommand.CommandText = cmd;
+
+                //eksekusi
+                myCommand.ExecuteNonQuery();
+
+                //Alert jika eksekusi berhasil
+                MessageBox.Show("Basisdata berhasil diperbarui", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                //Alert jika eksekusi gagal
+                MessageBox.Show(ex.ToString(), "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string myCmd = "INSERT INTO msprodi VALUES('"
+                 + txtIdProdi.Text + "','"
+                 + txtNamaProdi.Text + "','"
+                 + txtSingkatan.Text + "','"
+                 + txtKaProdi.Text + "','"
+                 + txtSekProdi.Text + "')";
+
+            UpdateDB(myCmd);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtIdProdi.Text = "";
+            txtNamaProdi.Text = "";
+            txtSingkatan.Text = "";
+            txtKaProdi.Text = "";
+            txtSekProdi.Text = "";
+        }
+    }
+}
